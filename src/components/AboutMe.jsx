403a9d48cbb7570d93fb196abe628baf6de5bd79
unknown <stp40@case.edu>
2022-12-05 20:17:22 -0500

    added resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
         <h1 id="aboutme-header"><sup className="superscript">//01&nbsp;</sup>About Me</h1>
         <div className="text-wrapper">
             <p id="aboutme-text">"An alumni from <strong>Case Western Reserve University</strong>, I originally planned on becoming a pharmacist through the school's pre-medical program. However, I discovered my passion for <strong>front-end development and design</strong>  
-            &nbsp;leading me to pursue further opportunities in the field. Driven to learn and develop my skills as a software engineer, I hope to use the insight and knowledge from my local peers to continue my growth through through self learning and motivation. 
+            &nbsp;leading me to pursue further opportunities in the field. Driven to learn and develop my skills as a software engineer, I hope to use the insight and knowledge from my local peers to continue my growth through self learning and motivation. 
             I am also currently studying and practicing my <strong>Japanese</strong>!"</p>
         </div>
     </div>
