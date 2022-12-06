a57c6cb80a5e534f23becdbb8266dd3ee892a147
unknown <stp40@case.edu>
2022-12-05 20:29:01 -0500

    download pdf

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
             <nav class="nav flex-sm-column">
                 <a className="nav-link" href="https://github.com/Steve-T-Pham" target="_blank">GITHUB</a>
                 <a className="nav-link" href="https://www.linkedin.com/in/steve-pham-stp40/" target="_blank">LINKEDIN</a>
-                <a className="nav-link" href="resume-steve-pham.pdf" target="_blank">RESUME</a>
+                <a className="nav-link" href="resume-steve-pham.pdf" download>RESUME</a>
             </nav>
         </div>
     );
